bd9b02f308033bf08bba087c8ded3b168fefb1ea
giwoong <dev.choi0409@gmail.com>
2024-11-18 00:14:50 +0900

    project setting

---
+++ src/test/java/com/moa/moabackend/MoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.moabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
