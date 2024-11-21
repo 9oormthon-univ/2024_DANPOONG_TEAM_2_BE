7207576664de6ecd6b643264ad00cf42f038355e
antegral <antegral@antegral.net>
2024-11-22 05:45:21 +0900

    docs(store): Add scrap API documentation

--- src/main/java/com/moa/moabackend/store/api/StoreControllerDocs.java
+++ src/main/java/com/moa/moabackend/store/api/StoreControllerDocs.java
@@ -1,8 +1,12 @@
 package com.moa.moabackend.store.api;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.moa.moabackend.global.template.RspTemplate;
+import com.moa.moabackend.member.domain.Member;
+import com.moa.moabackend.store.api.dto.request.SearchByLocationReqDto;
 import com.moa.moabackend.store.api.dto.request.StoreReqDto;
 import com.moa.moabackend.store.api.dto.response.StoreResDto;
 
@@ -26,7 +30,8 @@ public interface StoreControllerDocs {
         @Operation(summary = "상점 조회", description = "상점 ID로 상점 정보를 조회합니다.")
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "상점 조회 성공"),
-                        @ApiResponse(responseCode = "404", description = "상점을 찾을 수 없습니다.")
+                        @ApiResponse(responseCode = "404", description = "상점을 찾을 수 없습니다."),
+                        @ApiResponse(responseCode = "500", description = "상점 조회 중 문제가 발생했습니다.")
         })
         RspTemplate<StoreResDto> getStore(
                         @Parameter(name = "id", description = "상점 ID", in = ParameterIn.PATH) @PathVariable(name = "id") Long storeId);
@@ -35,7 +40,6 @@ public interface StoreControllerDocs {
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "상점 수정 성공"),
                         @ApiResponse(responseCode = "400", description = "해당하는 좌표로 가게를 찾지 못했습니다."),
-                        @ApiResponse(responseCode = "404", description = "수정할 상점을 찾을 수 없습니다."),
                         @ApiResponse(responseCode = "500", description = "좌표를 주소로 변환하는데 실패했습니다.")
         })
         RspTemplate<Boolean> putStore(
@@ -45,16 +49,43 @@ public interface StoreControllerDocs {
         @Operation(summary = "상점 삭제", description = "상점을 삭제합니다.")
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "상점 삭제 성공"),
-                        @ApiResponse(responseCode = "404", description = "삭제할 상점을 찾을 수 없습니다.")
+                        @ApiResponse(responseCode = "404", description = "상점을 찾을 수 없습니다."),
+                        @ApiResponse(responseCode = "500", description = "상점 삭제 중 문제가 발생했습니다.")
         })
         RspTemplate<Boolean> deleteStore(
                         @Parameter(name = "id", description = "상점 ID", in = ParameterIn.PATH) @PathVariable(name = "id") Long storeId);
 
-        // @Operation(summary = "주변 상점 검색", description = "지정된 좌표 반경 내의 상점들을 검색합니다.")
-        // @ApiResponses(value = {
-        // @ApiResponse(responseCode = "200", description = "상점 검색 성공")
-        // })
-        // RspTemplate<List<Number>> searchStoreByLocation(
-        // @RequestBody SearchByLocationReqDto searchByLocationReqDto
-        // );
+        @Operation(summary = "주소로 상점 검색", description = "지정된 주소 기준으로 상점들을 검색합니다.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "상점 검색 성공"),
+                        @ApiResponse(responseCode = "404", description = "검색된 상점이 없습니다."),
+                        @ApiResponse(responseCode = "500", description = "상점을 찾는 중 문제가 발생했습니다.")
+        })
+        RspTemplate<List<StoreResDto>> searchStoreByAddress(@RequestBody SearchByLocationReqDto searchByLocationReqDto);
+
+        @Operation(summary = "찜한 상점 목록 조회", description = "사용자가 찜한 상점 목록을 조회합니다.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "찜한 상점 목록 조회 성공"),
+                        @ApiResponse(responseCode = "404", description = "찜한 상점이 없거나, 유효한 유저가 아닙니다."),
+                        @ApiResponse(responseCode = "500", description = "찜한 상점 리스트를 가져오는 중 문제가 발생했습니다.")
+        })
+        RspTemplate<List<StoreResDto>> getScrapStoreList(@AuthenticationPrincipal Member member);
+
+        @Operation(summary = "상점 찜하기", description = "상점을 찜합니다.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "상점 찜하기 성공"),
+                        @ApiResponse(responseCode = "404", description = "상점을 찾을 수 없거나, 유효한 유저가 아닙니다."),
+                        @ApiResponse(responseCode = "500", description = "상점 찜하기 중 문제가 발생했습니다.")
+        })
+        RspTemplate<Boolean> scrapStore(@AuthenticationPrincipal Member member,
+                        @Parameter(name = "id", description = "상점 ID", in = ParameterIn.PATH) @PathVariable(name = "id") Long storeId);
+
+        @Operation(summary = "상점 찜하기 취소", description = "상점 찜하기를 취소합니다.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "상점 찜하기 취소 성공"),
+                        @ApiResponse(responseCode = "404", description = "찜하지 않은 상태에서 찜하기 취소를 시도했습니다."),
+                        @ApiResponse(responseCode = "500", description = "상점 찜하기 취소 처리 중 문제가 발생했습니다.")
+        })
+        RspTemplate<Boolean> unscrapStore(@AuthenticationPrincipal Member member,
+                        @Parameter(name = "id", description = "상점 ID", in = ParameterIn.PATH) @PathVariable(name = "id") Long storeId);
 }
