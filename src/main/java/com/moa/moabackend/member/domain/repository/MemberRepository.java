5e2bfcaebbdb1107c3ff3ae8d0014c2f8a6cef87
antegral <antegral@antegral.net>
2024-11-23 21:34:13 +0900

    refactor(member): remove unused imports

--- src/main/java/com/moa/moabackend/member/domain/repository/MemberRepository.java
+++ src/main/java/com/moa/moabackend/member/domain/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package com.moa.moabackend.member.domain.repository;
 
 import com.moa.moabackend.member.domain.Member;
-import com.moa.moabackend.store.domain.CertifiedType;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
